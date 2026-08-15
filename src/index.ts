f21dc0706c86836dab4677c11708936ff6a440b7
Neeraj Jakhar <neeraj.jakhar@gmail.com>
2026-08-15 16:06:18 +0530

    Add missing exports

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './update/types.js';
 export * from './update/update_environment.js';
 export * from './update/app_initialization_cubit.js';
 export * from './update/app_update_cubit.js';
+export * from './update/platform.js';
