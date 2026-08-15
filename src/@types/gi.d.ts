dd092808ddd3e9b39cb9d52d26f4e7c0e5183d2d
Neeraj Jakhar <neeraj.jakhar@gmail.com>
2026-08-15 11:51:31 +0530

    Fix build issue

---
+++ src/@types/gi.d.ts
@@ -0,0 +1,9 @@
+declare module 'gi://Soup' {
+    import Soup from '@girs/soup-3.0';
+    export = Soup;
+}
+
+declare module 'gi://GLib' {
+    import GLib from '@girs/glib-2.0';
+    export = GLib;
+}
