728ea952a31e62d45ba9243dc4cdea64455a0fd4
Neeraj Jakhar <neeraj.jakhar@gmail.com>
2026-08-16 22:44:27 +0530

    Fix fetch function

--- src/update/gnome_fetch.ts
+++ src/update/gnome_fetch.ts
@@ -1,40 +1,45 @@
-import Soup from 'gi://Soup';
-import GLib from 'gi://GLib';
-import { FetchWrapperResponse } from './fetch.js';
+import Soup from "gi://Soup?version=3.0";
+import GLib from "gi://GLib";
+import { FetchWrapperResponse } from "./fetch.js";
 
-export async function fetch(url: string, headers: Record<string, string> = {}): Promise<FetchWrapperResponse> {
-    const session = new Soup.Session();
-    const uri = GLib.Uri.parse(url, GLib.UriFlags.NONE);
-    const message = new Soup.Message({ method: 'GET', uri });
+export async function fetch(
+  url: string,
+  headers: Record<string, string> = {},
+): Promise<FetchWrapperResponse> {
+  const session = new Soup.Session();
+  const message = Soup.Message.new("GET", url);
 
-    const reqHeaders = message.get_request_headers();
-    for (const [key, value] of Object.entries(headers)) {
-        reqHeaders.append(key, value);
-    }
+  const reqHeaders = message.get_request_headers();
+  for (const [key, value] of Object.entries(headers["headers"])) {
+    reqHeaders.append(key, value);
+  }
 
-    const bytes = await session.send_and_read_async(
-        message,
-        GLib.PRIORITY_DEFAULT,
-        null
+  const bytes = await new Promise<GLib.Bytes>((resolve, reject) => {
+    session.send_and_read_async(
+      message,
+      GLib.PRIORITY_DEFAULT,
+      null,
+      (sess, result) => {
+        try {
+          resolve(sess!.send_and_read_finish(result) as GLib.Bytes);
+        } catch (e) {
+          reject(e);
+        }
+      },
     );
+  });
 
-    const statusCode = message.get_status();
-    if (statusCode !== 200) {
-        return {
-            ok: false,
-            status: statusCode,
-            json: async () => '',
-        };
-    }
+  const statusCode = message.get_status();
+  if (statusCode !== Soup.Status.OK) {
+    return { ok: false, status: statusCode, json: async () => "" };
+  }
 
-    const decoder = new TextDecoder('utf-8');
-    const text = decoder.decode(bytes.get_data()!);
-    const json = JSON.parse(text);
-    message.get_response_headers
+  const text = new TextDecoder().decode(bytes.get_data()!);
+  const json = JSON.parse(text);
 
-    return {
-        ok: true,
-        status: statusCode,
-        json: async () => json,
-    };
+  return {
+    ok: true,
+    status: statusCode,
+    json: async () => json,
+  };
 }
