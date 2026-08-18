811a0fffeb75166c38a585fb6b19f8753bc3bf96
Neeraj Jakhar <neeraj.jakhar@gmail.com>
2026-08-18 07:39:51 +0530

    Adding module definition

--- src/@types/gi.d.ts
+++ src/@types/gi.d.ts
@@ -1,9 +1,14 @@
-declare module 'gi://Soup' {
-    import Soup from '@girs/soup-3.0';
-    export = Soup;
+declare module "gi://Soup" {
+  import Soup from "@girs/soup-3.0";
+  export = Soup;
 }
 
-declare module 'gi://GLib' {
-    import GLib from '@girs/glib-2.0';
-    export = GLib;
+declare module "gi://Soup?version=3.0" {
+  import Soup from "@girs/soup-3.0";
+  export = Soup;
+}
+
+declare module "gi://GLib" {
+  import GLib from "@girs/glib-2.0";
+  export = GLib;
 }
